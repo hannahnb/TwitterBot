7ee213763e25a870e1562d024f4de51ec4be9d5c
hannahnb <hnormanbrown3@gatech.edu>
2023-01-04 15:20:16 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,65 @@
+// Our Twitter library
+var Twit = require('twit');
+
+// We need to include our configuration file
+var T = new Twit(require('./config.js'));
+
+// This is the URL of a search for the latest tweets on the '#uga' hashtag.
+var ugaSearch = {q: "uga", count: 10, result_type: "recent", expansions: "user.fields"}; 
+
+// This function finds the latest tweet with the #uga hashtag, and retweets it.
+function retweetLatest() {
+	T.get('search/tweets', ugaSearch, function (error, data) {
+	  // log out any errors and responses
+	  console.log(error, data);
+	  // If our search request to the server had no errors...
+	  if (!error) {
+	  	// ...then we grab the ID of the tweet we want to retweet...
+		var retweetId = data.statuses[0].id_str;
+		// ...and then we tell Twitter we want to retweet it!
+		T.post('statuses/retweet/' + retweetId,  { }, function (error, response) {
+			if (response) {
+				console.log('Success! Check your bot, it should have retweeted something.')
+			}
+			// If there was an error with our Twitter call, we print it out here.
+			if (error) {
+				console.log('There was an error with Twitter:', error);
+			}
+		})
+	  }
+	  // However, if our original search request had an error, we want to print it out here.
+	  else {
+	  	console.log('There was an error with your hashtag search:', error);
+	  }
+	});
+}
+
+var fs = require('fs');
+
+var b64content = fs.readFileSync('./THWG.jpg', { encoding: 'base64' })
+
+// first we must post the media to Twitter
+T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+  // now we can assign alt text to the media, for use by screen readers and
+  // other text-based presentations and interpreters
+  var mediaIdStr = data.media_id_string
+  var altText = "THWG."
+  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+  T.post('media/metadata/create', meta_params, function (err, data, response) {
+    if (!err) {
+      // now we can reference the media and post a tweet (media will attach to the tweet)
+      var params = { status: 'GO JACKETS #THWG', media_ids: [mediaIdStr] }
+
+      T.post('statuses/update', params, function (err, data, response) {
+        console.log(data)
+      })
+    }
+  })
+})
+
+// Try to retweet something as soon as we run the program...
+retweetLatest(); // retweets latest (WORKING)
+// ...and then every hour after that. Time here is in milliseconds, so
+// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
+setInterval(retweetLatest, 1000 * 60 * 60);
---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'RZfL8Y2nvYgubkCOf2GNr39WU',
+  consumer_secret:      'yoA6MnlZsXdYRNsy4DmaoGZ3v12gX1fZ3aet5aq90PozX1AKHL',
+  access_token:         '1587535929136201728-a6q7tMwufWiq9T0Yitr7Gqo1plyF41',
+  access_token_secret:  'j4M3gVxjfeIPydtWQLQCU1YRkkMQ59gHz9692ZLCbI7Qn'
+}
